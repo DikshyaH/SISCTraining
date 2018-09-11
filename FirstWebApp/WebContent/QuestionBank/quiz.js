e1d406f5445e2ae21573a972816b3a87d454bd58
7000017535 <7000017535@INSISCILT-3548.ap.sony.com>
2018-09-11 12:13:35 +0530

    The Final Quiz

--- FirstWebApp/WebContent/QuestionBank/quiz.js
+++ FirstWebApp/WebContent/QuestionBank/quiz.js
@@ -1,5 +1,5 @@
 var check=0;
-var questionTotalCount=2;
+var questionTotalCount=3;
 questionCount=0
 var ansList="";
 correctCount=0;
@@ -20,12 +20,12 @@ function startQuiz(){
               kelvis=("kelvi"+(questionCount+1));
               document.getElementById("questionNum").innerHTML="Question "+(questionCount+1)+":";
               question=quesObj[kelvis]["question"];
-              document.getElementById("kelvi").innerHTML=question
-              optCount=Number(quesObj[kelvis]["count"])
+              document.getElementById("kelvi").innerHTML=question;
+              optCount=Number(quesObj[kelvis]["count"]);
               correctAnswer=quesObj[kelvis]["answer"];
            
               for(var j=0;j<optCount;j++){
-                     optNumber='option'+(j+1)
+                     optNumber='option'+(j+1);
                      ansList+='<br>'+'<input type="radio" value=option'+(j+1)+' name="options">'+quesObj[kelvis][optNumber];
               }
        document.getElementById("quizSheet").innerHTML=ansList
@@ -46,17 +46,24 @@ function checkAnswer(){
                      alert('Correct answer :D');
                      correctCount+=1;
               }
-              else if( document.getElementById("quizSheet").options.value == -1 ){
+              else if( document.getElementById("quizSheet").options.value == "" ){
             	  		alert('Please enter an option');
-            	  		
-            	  		checkAnswer();
+            	  		return;
+//            	  		checkAnswer();		
               }
               else
-                  alert('Sorry! Wrong answer :(');
+            	
+          	document.getElementById("answer").innerHTML="Sorry! Wrong answer :(";
+            
+            	  alert('Sorry! Wrong answer :(');
 
               document.getElementById("quizSheet").innerHTML="";
               ansList="";
               questionCount+=1;
+             
+             
+              var answ = document.getElementById("answer");
+              answ.style.display='none';
               startQuiz();
        }
 }
@@ -64,6 +71,9 @@ function quizEndFunc(){
     document.getElementById("questionNum").innerHTML="Result";
     document.getElementById("kelvi").innerHTML="Your Score : "+correctCount+"/"+questionTotalCount;
     document.getElementById("quizSheet").innerHTML="";
+    var button = document.getElementById("answerSubmit");
+    button.style.display='none';
+   
     
 }
 
